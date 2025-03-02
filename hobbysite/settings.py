b931490c5fadeac188d6fd19497580381848ac16
glob-blob <gabriel.pacardo@student.ateneo.edu>
2025-03-02 21:46:40 +0800

    Changed DIRS in settings.py to read templates in base directory, and modified timezone to Asia/Manila

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'hobbysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Manila'
 
 USE_I18N = True
 
