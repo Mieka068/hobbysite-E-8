efd32bd9f75d2a06e93b876e4b0639bc35eafe77
Mieka068 <ramona.laciste@student.ateneo.edu>
2025-03-04 14:35:51 +0800

    Made the 2 views

--- commissions/views.py
+++ commissions/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from .models import Commission
 
-# Create your views here.
+class CommissionListView(ListView):
+    model = Commission
+    template_name = "commissions/list.html"
+    context_object_name = "commissions"
+
+class CommissionDetailView(DetailView):
+    model = Commission
+    template_name = "commissions/detail.html"
+    context_object_name = "commission"
+
+def commissions_list(request):
+    commissions = Commission.objects.order_by("created_on")  # Ensure ordering
+    return render(request, "commissions/list.html", {"commissions": commissions})
+
+def commission_detail(request, pk):
+    commission = get_object_or_404(Commission, pk=pk)
+    return render(request, "commissions/detail.html", {"commission": commission})
