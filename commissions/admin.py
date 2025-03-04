a9f391901f46bf56f94bc556c251c02f3a93d98b
Mieka068 <ramona.laciste@student.ateneo.edu>
2025-03-04 14:33:00 +0800

    Added an admin panel

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Commission, Comment
 
-# Register your models here.
+@admin.register(Commission)
+class CommissionAdmin(admin.ModelAdmin):
+    list_display = ("title", "people_required", "created_on", "updated_on")
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("commission", "entry", "created_on", "updated_on")
