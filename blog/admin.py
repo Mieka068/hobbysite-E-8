1e3c5617d5a0ba0d2971a9169e3ff46c31e6d28a
glob-blob <gabriel.pacardo@student.ateneo.edu>
2025-03-05 15:57:12 +0800

    Added admin view

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ArticleCategory, Article
+
+class ArticleInline(admin.TabularInline):
+    model = Article
+
+    search_fields = ('name', 'category', 'created_on', 'entry', )
+
+class ArticleCategoryAdmin(admin.ModelAdmin):
+    model = ArticleCategory
+    inlines = [ArticleInline]
+
+    search_fields = ('name', )
+
+    ('Details', {
+        'ingredients':
+            ('name', 'quantity')
+    })
+
+class ArticleAdmin(admin.ModelAdmin):
+    model=Article
+    fieldsets = [
+        ('Details', {
+            'fields': [
+                ('name', 'entry'), 'articlecategory'
+            ]
+        }),
+    ]
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(ArticleCategory, ArticleCategoryAdmin)
