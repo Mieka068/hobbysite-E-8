2d0049269bdfb8703307089cb771ae5a8d50df40
glob-blob <gabriel.pacardo@student.ateneo.edu>
2025-03-05 15:49:20 +0800

    Added paths to template in urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('/blog/articles', ArticleListView.as_view(), name='article_list'),
+    path('/blog/article/<int:pk>', ArticleDetailView.as_view(), name='article')
 ]
 
 app_name = 'blog'
