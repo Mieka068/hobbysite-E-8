fc12b8cc8691976d88373e9aac8184fff9303276
Andre-Villarin-ADMU <diniel.villarin@student.ateneo.edu>
2025-03-06 21:52:31 +0800

    Fixed views.py and list view formatting.

--- store/views.py
+++ store/views.py
@@ -5,9 +5,9 @@ def list_view_redirect(request):
     return redirect('list_view')
 
 def list_view(request):
-    all_products = ProductType.objects.all()
+    all_products = Product.objects.all()
     return render(request, 'list.html', {'all_products': all_products})
 
-def detail_view(request, product_id): #(request, id_):
-    product = get_object_or_404(id=product_id)
+def detail_view(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
     return render(request, 'detail.html', {'product': product})
