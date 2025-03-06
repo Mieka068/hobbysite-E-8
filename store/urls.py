c362de8ddd77ede9b5c40c40e1598ea520b4c687
Andre-Villarin-ADMU <diniel.villarin@student.ateneo.edu>
2025-03-06 21:28:43 +0800

    Fixed views and polished formatting a bit.

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,6 @@ from store import views
 
 urlpatterns = [
     path('', views.list_view_redirect, name='list_view_redirect'),
-    path('merchstore/items', views.list_view, name = 'list_view'),
-    path('merchstore/item', views.detail_view, name = 'detail_view'),
-    # path('merchstore/items/<int:id>/', views.detail_view, name = 'detail_view'),
+    path('merchstore/items', views.list_view, name='list_view'),
+    path('merchstore/item/<int:product_id>', views.detail_view, name='detail_view'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import redirect, render
-# from .models import ProductType, Product
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import ProductType, Product
 
 def list_view_redirect(request):
     return redirect('list_view')
 
 def list_view(request):
-    # all_products = ProductType.objects.all()
-    return render(request, 'list.html')
+    all_products = ProductType.objects.all()
+    return render(request, 'list.html', {'all_products': all_products})
 
-def detail_view(request): #(request, id_):
-    # products = Product.objects.get(id=id_)
-    return render(request, 'detail.html')
+def detail_view(request, product_id): #(request, id_):
+    product = get_object_or_404(id=product_id)
+    return render(request, 'detail.html', {'product': product})
