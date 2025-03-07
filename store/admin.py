d80e8577a769ab82419ddbcfa7b6078f58d3d30c
Andre-Villarin-ADMU <diniel.villarin@student.ateneo.edu>
2025-03-07 21:55:26 +0800

    Added store files.

---
+++ store/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from store import models
+from .models import ProductType, Product
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    model = ProductType
+    list_display = ['name', 'description']
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ['name', 'product_type', 'description', 'price']
+
+admin.site.register(ProductType, ProductTypeAdmin)
+admin.site.register(Product, ProductAdmin)
