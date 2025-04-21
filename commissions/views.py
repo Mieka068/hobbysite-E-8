cf951547c6743eff8600cd9d49644be0ca5aaa23
Mieka068 <ramona.laciste@student.ateneo.edu>
2025-04-22 05:59:32 +0800

    removed function-based views not used

--- commissions/views.py
+++ commissions/views.py
@@ -13,13 +13,3 @@ class CommissionDetailView(DetailView):
     model = Commission
     template_name = "commissions/detail.html"
     context_object_name = "commission"
-
-
-def commissions_list(request):
-    commissions = Commission.objects.order_by("created_on")  # Ensure ordering
-    return render(request, "commissions/list.html", {"commissions": commissions})
-
-
-def commission_detail(request, pk):
-    commission = get_object_or_404(Commission, pk=pk)
-    return render(request, "commissions/detail.html", {"commission": commission})
