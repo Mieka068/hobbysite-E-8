dc864c86ec7b9546c75eb667a102ecc562048194
Mieka068 <ramona.laciste@student.ateneo.edu>
2025-04-22 16:01:00 +0800

    added apply to job, create, and update views

--- commissions/views.py
+++ commissions/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
-from .models import Commission, JobApplication, Job
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .models import Commission, JobApplication, Job, Profile
 from django.db.models import Case, When, Value, IntegerField, Sum, Count, Q
 from django.contrib.auth.mixins import LoginRequiredMixin
 from user_management.models import Profile
-
+from django.contrib.auth.decorators import login_required
+from .forms import JobApplicationForm, CommissionForm, JobFormSet
+from django.contrib import messages
+from django.urls import reverse_lazy
 
 class CommissionListView(ListView):
     model = Commission
@@ -69,10 +72,88 @@ class CommissionDetailView(DetailView):
         if user.is_authenticated:
             profile = get_object_or_404(Profile, user=user)
             context["user_profile"] = profile
-            context["is_owner"] = commission.poster == profile
+            context["is_owner"] = commission.poster == user
         else:
             context["user_profile"] = None
             context["is_owner"] = False
 
         return context
 
+
+@login_required
+def apply_to_job(request, job_id):
+    job = get_object_or_404(Job, pk=job_id)
+    # Get Profile of the current user
+    profile = request.user.profile
+
+    # Check if already applied
+    if JobApplication.objects.filter(job=job, applicant=profile).exists():
+        messages.warning(request, "You have already applied to this job.")
+        return redirect('commissions:detail', pk=job.commission.id)
+
+    # Check if job is full
+    accepted_count = JobApplication.objects.filter(job=job, status='Accepted').count()
+    if accepted_count >= job.manpower_required:
+        messages.warning(request, "This job is already full.")
+        return redirect('commissions:detail', pk=job.commission.id)
+
+    # Save application
+    JobApplication.objects.create(job=job, applicant=profile, status='Pending')
+    messages.success(request, "You have successfully applied to the job.")
+    return redirect('commissions:detail', pk=job.commission.id)
+
+
+class CommissionCreateView(LoginRequiredMixin, CreateView):
+    model = Commission
+    form_class = CommissionForm
+    template_name = "commissions/commission_form.html"
+    success_url = reverse_lazy("commissions:list")
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        if self.request.POST:
+            data['job_formset'] = JobFormSet(self.request.POST)
+        else:
+            data['job_formset'] = JobFormSet()
+        return data
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        response = super().form_valid(form)
+        job_formset = self.get_context_data()['job_formset']
+        if job_formset.is_valid():
+            job_formset.instance = self.object
+            job_formset.save()
+        else:
+            return self.form_invalid(form)
+        return response
+
+
+class CommissionUpdateView(LoginRequiredMixin, UpdateView):
+    model = Commission
+    form_class = CommissionForm
+    template_name = "commissions/commission_form.html"
+    success_url = reverse_lazy("commissions:list")
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        if self.request.POST:
+            data['job_formset'] = JobFormSet(self.request.POST, instance=self.object)
+        else:
+            data['job_formset'] = JobFormSet(instance=self.object)
+        return data
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        job_formset = self.get_context_data()['job_formset']
+        if job_formset.is_valid():
+            job_formset.save()
+
+            # Check if all jobs are Full
+            if all(job.status == 'Full' for job in self.object.jobs.all()):
+                self.object.status = 'Full'
+                self.object.save()
+
+        else:
+            return self.form_invalid(form)
+        return response
