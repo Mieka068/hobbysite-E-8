7cccc13203ce17f55698ae6935c3e089ee9a0608
Mieka068 <ramona.laciste@student.ateneo.edu>
2025-04-22 11:39:18 +0800

    Replaced CommentAdmin with JobAdmin in line with the model changes

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from .models import Commission, Comment
+from .models import Commission, Job
 
 
 @admin.register(Commission)
 class CommissionAdmin(admin.ModelAdmin):
-    list_display = ("title", "people_required", "created_on", "updated_on")
+    list_display = ("title", "status", "created_on", "updated_on")
+    list_filter = ("status",)
+    search_fields = ("title",)
 
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ("commission", "entry", "created_on", "updated_on")
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ("commission", "role", "manpower_required", "status")
+    list_filter = ("status",)
+    search_fields = ("role",)
