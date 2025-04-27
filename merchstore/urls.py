fbc853b8fd8f245fba5670fcdd3e0cd8ea3b2869
Andre-Villarin-ADMU <diniel.villarin@student.ateneo.edu>
2025-04-27 16:46:40 +0800

    Fixed views.

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -11,6 +11,12 @@ app_name = 'merchstore'
 app_name = 'merchstore'
 
 urlpatterns = [
+<<<<<<< HEAD
     path('items/', views.list_view, name='list_view'),
     path('item/<int:product_id>/', views.detail_view, name='detail_view'),
+=======
+    path('', views.list_view_redirect, name='list_view_redirect'),
+    path('items/', views.list_view, name='list_view'),
+    path('merchstore/item/<int:product_id>/', views.detail_view, name='detail_view'),
+>>>>>>> 9b61377 (Fixed views.)
 ]
