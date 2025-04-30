eec751236f5d482ab9a87f9b1558fbd510b2eb11
Janelle Chloe Francisco <janelle.francisco@student.ateneo.edu>
2025-05-01 02:30:40 +0800

    Add author field to Article model, create Comment model

--- wiki/models.py
+++ wiki/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+#from user_management.models import Profile
 
 
 class ArticleCategory(models.Model):
@@ -18,6 +19,10 @@ class ArticleCategory(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
+    #author = models.ForeignKey(
+    #    Profile,
+    #    on_delete=models.SET_NULL,
+    #)
     category = models.ForeignKey(
         ArticleCategory,
         on_delete=models.SET_NULL,
@@ -25,6 +30,7 @@ class Article(models.Model):
         related_name='articles',
     )
     entry = models.TextField
+    header_image = models.ImageField
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -33,3 +39,20 @@ class Article(models.Model):
 
     class Meta:
         ordering = ['-created_on']
+
+
+class Comment(models.Model):
+    #author = models.ForeignKey(
+    #    Profile,
+    #    on_delete=models.SET_NULL
+    #)
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE
+    )
+    entry = models.TextField
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_on']
