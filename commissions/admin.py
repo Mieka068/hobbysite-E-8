74b4908a6cc6e73d0b41a6a4d22a0ae236d150ef
Mieka068 <ramona.laciste@student.ateneo.edu>
2025-05-01 18:04:24 +0800

    Added the Job applications

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Commission, Job
+from .models import Commission, Job, JobApplication
 
 
 @admin.register(Commission)
@@ -14,3 +14,11 @@ class JobAdmin(admin.ModelAdmin):
     list_display = ("commission", "role", "manpower_required", "status")
     list_filter = ("status",)
     search_fields = ("role",)
+
+
+@admin.register(JobApplication)
+class JobApplicationAdmin(admin.ModelAdmin):
+    list_display = ('applicant', 'job', 'status', 'applied_on')
+    list_filter = ('status', 'applied_on')
+    search_fields = ('applicant__display_name', 'job__role')
+    ordering = ('status', '-applied_on')
