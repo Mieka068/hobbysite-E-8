008d9a3522c67111da6bddd83035e067e6825ae4
Andre-Villarin-ADMU <diniel.villarin@student.ateneo.edu>
2025-05-08 19:11:48 +0800

    Fixed store page after being redirected from  homepage.

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -2,21 +2,11 @@ from django.http import HttpResponseRedirect
 from django.urls import include, path
 
 from merchstore import views
-<<<<<<< HEAD
-
-app_name = 'merchstore'
-=======
->>>>>>> 8d2415d (Renamed app from store to merchstore and made changes to relevant files.)
 
 app_name = 'store'
 
 urlpatterns = [
-<<<<<<< HEAD
+    path('', views.list_view, name='list_view'),
     path('items/', views.list_view, name='list_view'),
-    path('item/<int:product_id>/', views.detail_view, name='detail_view'),
-=======
-    path('', views.list_view_redirect, name='list_view_redirect'),
-    path('items/', views.list_view, name='list_view'),
-    path('merchstore/item/<int:product_id>/', views.detail_view, name='detail_view'),
->>>>>>> 9b61377 (Fixed views.)
+    path('items/item/<int:product_id>/', views.detail_view, name='detail_view'),
 ]
