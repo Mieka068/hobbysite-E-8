c5635ff1d7c871a0b6bd445987b931b5d9fcd746
Andre-Villarin-ADMU <diniel.villarin@student.ateneo.edu>
2025-05-08 20:54:43 +0800

    Made new models with some revisions.

--- merchstore/models.py
+++ merchstore/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.urls import reverse
+
+from user_management.models import Profile
+
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
@@ -11,16 +14,47 @@ class ProductType(models.Model):
         ordering = ['name']
     
 class Product(models.Model):
+    status_options = [
+        ('Available', 'Available'),
+        ('On sale', 'On sale'),
+        ('Out of stock', 'Out of stock')
+        ]
+    
     name = models.CharField(max_length=255)
 
     product_type = models.ForeignKey(
         ProductType,
         on_delete=models.SET_NULL,
         null = True,
-        related_name = 'category')
-            
+        related_name = 'products_of_type'
+    )
+
+    owner = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='products_of_owner'
+    )
+    
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    price = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2
+    )
+
+    stock = models.PositiveIntegerField()
+
+    status = models.CharField(
+        max_length = 20,
+        choices = status_options,
+        default = 'Available'
+    )
+
+    def save(self, *args, **kwargs):
+        if self.stock == 0:
+            self.status = 'Out of stock'
+
+        super(Product, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -30,3 +64,35 @@ class Product(models.Model):
 
     class Meta:
         ordering = ['name']
+
+class Transaction(models.Model):
+    transaction_status = [
+        ("On Cart", "On Cart"),
+        ("To Pay", "To Pay"),
+        ("To Ship", "Cancelled"),
+        ("To Receive", "To Receive"),
+        ("Delivered", "Delivered"),
+    ]
+    buyer = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        related_name="transactions_of_buyer"
+    ) 
+    
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.SET_NULL,
+        related_name="transactions_of_product"
+    ) 
+    
+    amount = models.PositiveIntegerField()
+
+    status = models.CharField(
+        max_length=20,
+        choices=transaction_status,
+        default="On Cart"
+    )
+
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
