e23b8b6c4a21968932e7a867959b67c4a6550bf9
Andre-Villarin-ADMU <diniel.villarin@student.ateneo.edu>
2025-05-10 02:13:20 +0800

    Finalized product list view.

--- merchstore/admin.py
+++ merchstore/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from merchstore import models
-from .models import ProductType, Product
+from .models import ProductType, Product, Transaction
 
 class ProductTypeAdmin(admin.ModelAdmin):
     model = ProductType
@@ -8,7 +8,17 @@ class ProductTypeAdmin(admin.ModelAdmin):
 
 class ProductAdmin(admin.ModelAdmin):
     model = Product
-    list_display = ['name', 'product_type', 'description', 'price']
+    list_display = ['name', 'product_type', 
+                    'description', 'price'
+                    ]
 
+class TransactionAdmin(admin.ModelAdmin):
+    model = Transaction
+    list_display = ['transaction_status', 'buyer', 
+                    'product', 'amount',
+                    'status', 'created_on'
+                    ]
+    
 admin.site.register(ProductType, ProductTypeAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Transaction, TransactionAdmin)
--- merchstore/views.py
+++ merchstore/views.py
@@ -1,3 +1,4 @@
+from .models import Profile
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 
@@ -5,8 +6,24 @@ from .models import ProductType, Product
 from .forms import AddProductForm
 
 def list_view(request):
-    all_products = Product.objects.all()
-    return render(request, 'list.html', {'all_products': all_products})
+    user = request.user
+    if user.is_authenticated:
+        try:
+            user_profile = Profile.objects.get(user=user)
+        except Profile.DoesNotExist:
+            user_profile = None
+        
+        owned_products = Product.objects.filter(owner=user_profile)
+        other_products = Product.objects.exclude(owner=user_profile)
+        ctx = {
+            'owned_products': owned_products,
+            'other_products': other_products,
+        }
+    else:
+        ctx = {
+            'all_products': Product.objects.all()
+        }
+    return render(request, 'list.html', ctx)
 
 def detail_view(request, product_id):
     product = get_object_or_404(Product, id=product_id)
@@ -17,7 +34,9 @@ def add_product_view(request):
     if request.method == 'POST':
         form = AddProductForm(request.POST)
         if form.is_valid():
-            form.save()
+            product = form.save(commit=False)
+            product.owner = request.user.profile
+            product.save()
             return redirect('store:list_view')
     else:
         form = AddProductForm()
