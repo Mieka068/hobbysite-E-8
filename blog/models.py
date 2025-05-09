46eac1607f74127eee8b4de2edfa6bb1ce5738b7
glob-blob <gabriel.pacardo@student.ateneo.edu>
2025-05-09 22:39:38 +0800

    Added Comment Model and renamed foreignkey related_name of Article from 'categories' to 'articles'

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from user_management.models import Profile
 
 
 class ArticleCategory(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField
+
 
     class Meta:
         ordering = ['name']
@@ -15,13 +17,19 @@ class ArticleCategory(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        related_name='author'
+    )
     category = models.ForeignKey(
         ArticleCategory,
         on_delete=models.SET_NULL,
         null=True,
-        related_name='category'
+        related_name='articles'
         )
     entry = models.TextField(null=True)
+    header_img = models.ImageField(upload_to='headers/')
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -31,8 +39,34 @@ class Article(models.Model):
     def get_absolute_url(self):
         return reverse('blog:article', args=[str(self.pk)])
 
+
     class Meta:
         ordering = ['-created_on']
         unique_together = ['title']
         verbose_name = 'article'
         verbose_name_plural = 'articles'
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        related_name='author'
+    )
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ['-created_on']
+        verbose_name = 'comment'
+        verbose_name_plural = 'comments'
+
+
+
