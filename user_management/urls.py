c107f9340432245a31f14c49c397764e02cadbe4
glob-blob <gabriel.pacardo@student.ateneo.edu>
2025-05-10 10:08:07 +0800

    Moved login, logout, and register urls to Accounts app

--- user_management/urls.py
+++ user_management/urls.py
@@ -6,11 +6,6 @@ app_name = "user_management"
 urlpatterns = [
     #keep in Profile App
     path('', views.homepage, name='homepage'),
-
-    # transfer to Accounts app
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register_view, name='register'),
     
     #keep in Profile App
     path('profile/', views.profile_update, name='profile'),
