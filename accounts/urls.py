ce05eb64f528984bdf66d60f31572bdc157646f4
glob-blob <gabriel.pacardo@student.ateneo.edu>
2025-05-12 01:43:26 +0800

    Fixed password reset login flow

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('register/', register_view, name='register'),
     path('reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),  
-    path('reset/confirm', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),  
+    path('reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),  
     path('reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),  
 ]
