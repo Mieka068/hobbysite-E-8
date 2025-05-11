e7257e601ca0c5b8c906109a1b749d047616377f
glob-blob <gabriel.pacardo@student.ateneo.edu>
2025-05-12 01:44:32 +0800

    Made profile email field to also set User email

--- user_management/models.py
+++ user_management/models.py
@@ -6,5 +6,11 @@ class Profile(models.Model):
     display_name = models.CharField(max_length=63)
     email = models.EmailField()
 
+    def save(self, *args, **kwargs):
+        if self.email and self.user.email != self.email:
+            self.user.email = self.email
+            self.user.save()
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.display_name or self.user.username
