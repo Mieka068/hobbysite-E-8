d437e277be25c672884dc59a4821564dee8cfbe6
Mieka068 <ramona.laciste@student.ateneo.edu>
2025-05-11 21:21:26 +0800

    Added dashboard view

--- user_management/views.py
+++ user_management/views.py
@@ -5,8 +5,9 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from .forms import ProfileForm
 from .models import Profile
+from commissions.models import Commission, JobApplication
 
-# Registration View
+# Registration View - transfer to Accounts app
 def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -20,7 +21,7 @@ def register_view(request):
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
 
-# Custom Login View
+# Custom Login View - Transfer to Accounts app
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
@@ -32,13 +33,13 @@ def login_view(request):
         form = AuthenticationForm()
     return render(request, 'registration/login.html', {'form': form})
 
-# Custom Logout View
+# Custom Logout View - transfer to Accounts app
 @login_required
 def logout_view(request):
     logout(request)
     return redirect('user_management:login')
 
-# Profile Update View
+# Profile Update View - Keep in Profile app
 @login_required
 def profile_update(request):
     profile = get_object_or_404(Profile, user=request.user)
@@ -53,6 +54,24 @@ def profile_update(request):
 
     return render(request, 'user_management/profile_form.html', {'form': form})
 
-# Homepage View
+# Homepage View - Keep in Profile app
 def homepage(request):
     return render(request, 'user_management/homepage.html')
+
+@login_required
+def dashboard_view(request):
+    user = request.user
+
+    # Commissions created by this user
+    commissions_created = Commission.objects.filter(poster=user)
+
+    # Commissions joined (where this user's profile is accepted)
+    accepted_apps = JobApplication.objects.filter(applicant=user.profile, status="Accepted")
+    commissions_joined = Commission.objects.filter(
+        jobs__applications__in=accepted_apps
+    ).distinct()
+
+    return render(request, 'user_management/dashboard.html', {
+        'commissions_created': commissions_created,
+        'commissions_joined': commissions_joined,
+    })
