3d89474f67109d36f334bf6800b842b9b10da8bd
JoshGaffud <joshua.gaffud@student.ateneo.edu>
2025-05-13 18:09:13 +0800

    Update models.py

--- forum/models.py
+++ forum/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.urls import reverse
 
 
@@ -10,8 +11,8 @@ class ThreadCategory(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('forum:forum_thread', args=[str(self.id)])
-    
+        return reverse('forum:thread_detail', args=[str(self.id)])
+
     class Meta:
         ordering = ['name']
 
@@ -19,23 +20,46 @@ class ThreadCategory(models.Model):
 class Thread(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
         null=True,
-        related_name='posts',
-        )
+        related_name='threads'
+    )
     category = models.ForeignKey(
         ThreadCategory,
         on_delete=models.SET_NULL,
         null=True,
-        related_name='posts',
-        )
+        related_name='threads'
+    )
     entry = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
-    
+
     class Meta:
         ordering = ['-created_on']
-        
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='comments'
+    )
+    thread = models.ForeignKey(
+        Thread,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.thread}'
+
+    class Meta:
+        ordering = ['created_on']
