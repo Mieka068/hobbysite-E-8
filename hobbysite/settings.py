b3790ddbd2902daf8a56b6120e22f751fde9f4df
glob-blob <gabriel.pacardo@student.ateneo.edu>
2025-05-19 12:03:46 +0800

    removed staticfiles dirs

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -148,7 +148,6 @@ USE_TZ = True
 
 STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_ENDPOINT_URL}/static/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
